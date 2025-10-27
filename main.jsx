037f711003b20b3dbcb07319263995a03880f1bc
adrianasrb09 <sofirojs2009@gmail.com>
2025-10-27 16:14:23 -0500

    Initialize main.jsx with React entry point

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PetiteSucreeStore from "./Aplicación.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PetiteSucreeStore />
+  </React.StrictMode>
+);
